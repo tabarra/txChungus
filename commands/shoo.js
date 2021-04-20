449f52b5f5def9cbcb4a91b2ad1bd283952e6c78
LedAndris <ledererandras2004@gmail.com>
2021-04-20 22:13:52 +0200

    fuck u jesus

--- commands/shoo.js
+++ commands/shoo.js
@@ -20,9 +20,6 @@ module.exports = {
         if (!mention || !args[1]) return message.reply('Please use the correct command format. `!shoo @mention tx/fx/res/zap/pt`');
         if (mention.user.id === message.author.id) return message.reply('lol what'); //user trying to shoo himself
         if (mention.user.id === message.client.user.id) return message.reply('dont think so'); //user trying to shoo chungus
-        if (GlobalData.mutes.find(mute => mute.id == mention.id)) {
-            GlobalData.removeMute(mention.id);
-        }
 
         const location = args[1]
         if (locations.has(location)) {
